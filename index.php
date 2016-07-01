7293fdd554a523366def665f4730e768efd9ea5f
bryan delfin <bryan.delfin@cpe.pe>
2016-07-01 12:54:41 -0500

    edit index.php

--- index.php
+++ index.php
@@ -2,32 +2,78 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>    
+	<title>Document</title> 
+	<link rel="stylesheet" href="bower_components/angular-spinkit/build/angular-spinkit.min.css" /> 
 	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.6/angular.min.js"></script>
+	<script src="bower_components/angular-spinkit/build/angular-spinkit.min.js"></script>  
 </head>
 <body ng-app='httpExample'>
 	<div ng-controller="PersonsCtrl">
-		<div>
-			<ul >
-				<li ng-repeat="per in persons">
-					<div> nombre : {{ per.nombre }} , ciudad {{ per.ciudad }} </div>
-				</li>
-			</ul>
+
+		<!-- click ajax loader spinkit -->
+
+		<button ng-click="sendAjax();">Send Data</button>
+		<div  style="background-color: #23b7e5;" ng-show="loader" >
+			<rotating-plane-spinner></rotating-plane-spinner>
+			<circle-spinner></circle-spinner>
+			<cube-grid-spinner></cube-grid-spinner>
+		</div>
+		
+		<div ng-hide="true">
+			<div>Mostrar
+				<button ng-click="loaderFn();">
+					Click loader
+				</button>
+				<div ng-show="State_loader">
+					<circle-spinner></circle-spinner>
+				</div>
+			</div>
+			<div>
+				<ul >
+					<li ng-repeat="per in persons">
+						<div> nombre : {{ per.nombre }} , ciudad {{ per.ciudad }} </div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<script>
-		var app = angular.module('httpExample', []);
+		var app = angular.module('httpExample', ['angular-spinkit']);
 		app.controller('PersonsCtrl', function($scope, personService){
+			
+			$scope.loader = false ;
+			$scope.sendAjax = function () {
+				$scope.loader  = true ;
+				personService.getPersons().then(function(response){
+					console.log('data', response);
+					$scope.persons = response.data;
+					$scope.loader = false ;
+				}, function (error) {
+					console.log('data error', error);
+					$scope.loader = false ;
+				});
+			};
 
-			personService.getPersons().then(function(response){
-				$scope.persons = response.data;
-			});
+
+
+
+			/*
+				$scope.State_loader = false;
+
+				$scope.loaderFn = function () {
+					$scope.State_loader = true ;
+				};
+
+				personService.getPersons().then(function(response){
+					$scope.persons = response.data;
+				});
+			*/
 		});
 
 		app.factory('personService', function($http) {
 
 			var getPersons = function() {
-				return $http.get('/api/persona.php');
+				return $http.get('https://randomuser.me/api/');
 			};
 
 			return {
